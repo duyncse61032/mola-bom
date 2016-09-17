1a295e18c79bf72d0110ce4d1793acd4152fea80
Phuc Tran <phuctran93@hotmail.com>
2016-09-17 19:21:35 +0700

    feat: Using spring framework’s model and view pattern
    	1. Configuring View Resolution
    	2. Configuring View Name Translation
    	2. Configuring Message Converters
    	2. Configuring Content Negotiation

--- source/production/java/vn/edu/fpt/mola/bom/config/RootContextConfiguration.java
+++ source/production/java/vn/edu/fpt/mola/bom/config/RootContextConfiguration.java
@@ -1,8 +1,14 @@
 package vn.edu.fpt.mola.bom.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Controller;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Configuration
 @ComponentScan(
@@ -11,4 +17,22 @@ import org.springframework.stereotype.Controller;
 )
 public class RootContextConfiguration
 {
+    @Bean
+    public ObjectMapper objectMapper()
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.findAndRegisterModules();
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE,
+                false);
+        return mapper;
+    }
+
+    @Bean
+    public Jaxb2Marshaller jaxb2Marshaller()
+    {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setPackagesToScan(new String[] { "com.wrox.site" });
+        return marshaller;
+    }
 }
--- source/production/java/vn/edu/fpt/mola/bom/config/ServletContextConfiguration.java
+++ source/production/java/vn/edu/fpt/mola/bom/config/ServletContextConfiguration.java
@@ -1,17 +1,126 @@
 package vn.edu.fpt.mola.bom.config;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.http.converter.xml.SourceHttpMessageConverter;
+import org.springframework.oxm.Marshaller;
+import org.springframework.oxm.Unmarshaller;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.RequestToViewNameTranslator;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.DefaultRequestToViewNameTranslator;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
-@EnableWebMvc    // facilitate mapping requests to controller methods
+@EnableWebMvc
 @ComponentScan(
         basePackages = "vn.edu.fpt.mola.bom.site",
         useDefaultFilters = false,
         includeFilters = @ComponentScan.Filter(Controller.class)
 )
-public class ServletContextConfiguration
+public class ServletContextConfiguration extends WebMvcConfigurerAdapter
 {
+    @Inject ObjectMapper objectMapper;
+    @Inject Marshaller marshaller;
+    @Inject Unmarshaller unmarshaller;
+
+    /**
+     * Configuring Message Converters
+     */
+    @Override
+    public void configureMessageConverters(
+            List<HttpMessageConverter<?>> converters) {
+    	
+        converters.add(new ByteArrayHttpMessageConverter());
+        converters.add(new StringHttpMessageConverter());
+        converters.add(new FormHttpMessageConverter());
+        converters.add(new SourceHttpMessageConverter<>());
+
+        MarshallingHttpMessageConverter xmlConverter =
+                new MarshallingHttpMessageConverter();
+        xmlConverter.setSupportedMediaTypes(Arrays.asList(
+                new MediaType("application", "xml"),
+                new MediaType("text", "xml")
+        ));
+        xmlConverter.setMarshaller(this.marshaller);
+        xmlConverter.setUnmarshaller(this.unmarshaller);
+        converters.add(xmlConverter);
+
+        MappingJackson2HttpMessageConverter jsonConverter =
+                new MappingJackson2HttpMessageConverter();
+        jsonConverter.setSupportedMediaTypes(Arrays.asList(
+                new MediaType("application", "json"),
+                new MediaType("text", "json")
+        ));
+        jsonConverter.setObjectMapper(this.objectMapper);
+        converters.add(jsonConverter);
+    }
+
+    /**
+     * Configuring Content Negotiation
+     */
+    @Override
+    public void configureContentNegotiation(
+            ContentNegotiationConfigurer configurer)
+    {
+        configurer.favorPathExtension(true)
+                .favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useJaf(false)
+                .defaultContentType(MediaType.APPLICATION_XML)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("json", MediaType.APPLICATION_JSON);
+    }
+    
+    /**
+     * Configuring View Resolution
+     * @return
+     */
+    @Bean
+    public ViewResolver viewResolver()
+    {
+        /*  The InternalResourceView and JstlView implement traditional JSP and 
+            JSTL-enhanced-JSP views, respectively. They take care of translating model attributes to 
+            request attributes and forwarding the request on to the proper JSP. */
+        InternalResourceViewResolver resolver =
+                new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/jsp/view/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    /**
+     * Configuring View Name Translation
+     * @return
+     */
+    @Bean
+    public RequestToViewNameTranslator viewNameTranslator()
+    {
+    	/*  Strips off the web application context URL and any file extension
+    	    at the end of the URL. What remains of the URL becomes the view name.
+    	    For example, the URL http://localhost:8080/mvc/foo turns into the view name foo,
+    	    whereas the URL http://localhost:8080/mvc/foo/bar.html turns into 
+            the view name foo/bar. */
+        return new DefaultRequestToViewNameTranslator();
+    }
 }
